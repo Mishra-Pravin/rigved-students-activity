8cdbb1f046bdd89146150ebde91bce49a59ea7af
Pravin <pravinmishra163@gmail.com>
2022-04-18 05:54:20 +0530

    activity completed

---
+++ Angular_CLI/Day_39_/nested-routers/src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';  
+import { Routes, RouterModule } from '@angular/router';  
+    
+import { HomeComponent } from './home/home.component';  
+import { PostsComponent } from './posts/posts.component';  
+import { CreateComponent } from './posts/create/create.component';
+import { DetailComponent } from './posts/detail/detail.component';
+
+const routes: Routes = [  
+  {  
+      path: '',  
+      component: HomeComponent  
+  },  
+  {  
+      path: 'posts',  
+      children: [  
+        {  
+          path: '',  
+          component: PostsComponent  
+        },  
+        {  
+            path: 'create',  
+            component: CreateComponent  
+        },  
+        {  
+            path: 'detail',  
+            component: DetailComponent  
+        }  
+    ]  
+  }  
+];  
+     
+@NgModule({  
+  imports: [RouterModule.forRoot(routes)],  
+  exports: [RouterModule]  
+})  
+export class AppRoutingModule { }  
---
+++ Angular_CLI/Day_39_/nested-routers/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PostsComponent } from './posts/posts.component';
+import { CreateComponent } from './posts/create/create.component';
+import { DetailComponent } from './posts/detail/detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PostsComponent,
+    CreateComponent,
+    DetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
